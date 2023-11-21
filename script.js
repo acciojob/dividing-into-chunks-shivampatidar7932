9b1b21d3392b37d82f22f60b8f6cee3cdf2a3b96
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-21 17:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+    if (!Array.isArray(arr) || typeof n !== 'number' || n <= 0) {
+        console.error("Invalid input. Please provide a valid array and a positive integer.");
+        return;
+    }
+
+    const result = [];
+    let currentSubarray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (currentSubarray.reduce((sum, num) => sum + num, 0) + arr[i] <= n) {
+            currentSubarray.push(arr[i]);
+        } else {
+            result.push([...currentSubarray]);
+            currentSubarray = [arr[i]];
+        }
+    }
+
+    if (currentSubarray.length > 0) {
+        result.push(currentSubarray);
+    }
+
+    return result;
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+const userN = prompt("Enter n: ");
+alert(JSON.stringify(divide(arr, parseInt(userN, 10))));
