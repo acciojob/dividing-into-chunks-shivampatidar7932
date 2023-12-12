020fc5bed03ddd57210862e6d9619ef9629efca4
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-12 15:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
+const arr=[1,2,3,4,1,0,2,2];
 const divide = (arr, n) => {
-  const result = [];
-  let currentSubarray = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    const currentElement = arr[i];
-
-    if (currentSubarray.reduce((sum, num) => sum + num, 0) + currentElement <= n) {
-      currentSubarray.push(currentElement);
-    } else {
-      result.push([...currentSubarray]);
-      currentSubarray = [currentElement];
-    }
-  }
-
-  // Add the last subarray
-  if (currentSubarray.length > 0) {
-    result.push([...currentSubarray]);
-  }
-
-  return result;
+  let trackingSum=0,ans=[],subArr=[];
+	for(let i=0;i<arr.length;i++){
+		if(trackingSum+arr[i]<=n){
+			subArr.push(arr[i]);
+			trackingSum +=arr[i];
+		}
+		else{
+			ans.push(subArr);
+			subArr=[arr[i]];
+			trackingSum=arr[i];
+		}
+	}
+	ans.push(subArr);
+	return ans;
 };
 
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-const n = parseInt(prompt("Enter n: "), 10);
+
+const n = prompt("Enter n:");
 alert(JSON.stringify(divide(arr, n)));
